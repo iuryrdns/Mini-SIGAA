97c0d9a8ddc49e7aec2c96ba006ef2271f5ff77d
Iury <iury32211@outlook.com>
2026-01-15 16:09:25 -0300

    Refatora o modelo Turma, adicionando campos e funções de acesso

--- src/Models/Turma.hs
+++ src/Models/Turma.hs
@@ -1,12 +1,42 @@
-module Models.Turma where
+module Models.Turma(Turma, getCodigoTurma, getAlunosTurma, getDisciplinaTurma, getHorarioTurma, getProfessorTurma, adicionarAlunoTurma, criarTurma) where
 
-import Models.Aluno (Aluno)
-import Models.Professor (Professor)
+import Models.Professor
+import Models.Aluno
+import Models.Disciplina
 
-data Turma = Turma
-  { codigo :: String,
-    professor :: Professor,
-    horario :: String,
-    alunos :: [Aluno]
-  }
-  deriving (Show)
+data Turma = Turma{ 
+  _codigo :: Int,
+  _professor :: Professor,
+  _disciplina :: Disciplina,
+  _horario :: String,
+  _alunos :: [Aluno]
+  } deriving (Show, Read, Eq)
+
+criarTurma :: Int -> Professor -> Disciplina -> String -> Turma 
+criarTurma codigo professor disciplina horario = Turma {
+  _codigo = codigo,
+  _professor = professor,
+  _disciplina = disciplina,
+  _horario = horario,
+  _alunos = []
+}
+
+adicionarAlunoTurma :: Turma -> Aluno -> Turma
+adicionarAlunoTurma turma aluno = turma {
+  _alunos = _alunos turma ++ [aluno]
+}
+
+getCodigoTurma :: Turma -> Int
+getCodigoTurma = _codigo
+
+getProfessorTurma :: Turma -> Professor
+getProfessorTurma = _professor
+
+getDisciplinaTurma :: Turma -> Disciplina
+getDisciplinaTurma = _disciplina
+
+getHorarioTurma :: Turma -> String
+getHorarioTurma = _horario
+
+getAlunosTurma :: Turma -> [Aluno]
+getAlunosTurma = _alunos
