4923eee69f7b143d2d22daa9bb681a16bea25f0e
Iury <iury32211@outlook.com>
2026-01-15 16:09:08 -0300

    Refatora o modelo Professor, adicionando campos e funções de acesso

--- src/Models/Professor.hs
+++ src/Models/Professor.hs
@@ -1,7 +1,30 @@
-module Models.Professor where
+module Models.Professor(Professor, getMatriculaProfessor, getDepartamentoProfessor, getFormacaoProfessor, getNomeProfessor, criarProfessor) where
 
-data Professor = Professor
-  { cpf :: String, -- Arrumar um identificador melhor
-    nome :: String
-  }
-  deriving (Show)
+data Professor = Professor{ 
+  _matricula :: Int, 
+  _nome :: String,
+  _departamento :: String,
+  _formacao :: String
+  } deriving (Show, Read, Eq)
+
+criarProfessor :: Int -> String -> String -> String -> Professor
+criarProfessor matricula nome departamento formacao = Professor {
+  _matricula = matricula,
+  _nome = nome,
+  _departamento = departamento,
+  _formacao = formacao
+}
+
+
+getMatriculaProfessor :: Professor -> Int
+getMatriculaProfessor = _matricula
+
+getNomeProfessor :: Professor -> String
+getNomeProfessor = _nome
+
+getDepartamentoProfessor :: Professor -> String
+getDepartamentoProfessor = _departamento
+
+getFormacaoProfessor :: Professor -> String
+getFormacaoProfessor = _formacao
+
