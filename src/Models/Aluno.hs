d118d1433235a126d6ef8a04d94f622f9378aec8
Iury <iury32211@outlook.com>
2026-01-15 16:08:57 -0300

    Adiciona funções de acesso e construtor para o modelo Aluno

--- src/Models/Aluno.hs
+++ src/Models/Aluno.hs
@@ -1,9 +1,34 @@
-module Models.Aluno (Aluno) where
+module Models.Aluno (Aluno, getMatriculaAluno, getCraAluno, getCursoAluno, getNomeAluno, criarAluno) where
 
-data Aluno = Aluno
-  { matricula :: Int,
-    nomr :: String,
-    curso :: String,
-    cra :: Float
-  }
-  deriving (Show)
+import qualified Data.Map as Map
+data Aluno = Aluno {
+  _matricula :: Int,
+  _nome :: String,
+  _curso :: String,
+  _cra :: Float,
+  _notas :: Map.Map Int Int
+  } deriving (Show, Read, Eq)
+
+criarAluno :: Int -> String -> String -> Float -> Aluno
+criarAluno matricula nome curso cra = Aluno {
+  _matricula = matricula,
+  _nome = nome,
+  _curso = curso,
+  _cra = cra,
+  _notas = Map.empty
+}
+
+getMatriculaAluno :: Aluno -> Int
+getMatriculaAluno = _matricula
+
+getNomeAluno :: Aluno -> String
+getNomeAluno = _nome
+
+getCursoAluno :: Aluno -> String
+getCursoAluno = _curso
+
+getCraAluno :: Aluno -> Float
+getCraAluno = _cra
+
+
+
