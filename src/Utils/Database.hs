748919151b1d60e9966a12e0acb20a44b431817a
Iury <iury32211@outlook.com>
2026-01-16 13:46:33 -0300

    Refatora o módulo Database, implementando funções para carregar e salvar o sistema

--- src/Utils/Database.hs
+++ src/Utils/Database.hs
@@ -1,2 +1,35 @@
-module Utils.Database where
+module Utils.Database (carregarSistema, salvarSistema) where
 
+import Sistema (Sistema, sistemaVazio, dbPath)
+import System.Directory (doesFileExist, renameFile)
+import System.IO (withFile, IOMode(..), hGetContents)
+import Text.Read (readMaybe)
+
+carregarSistema :: IO Sistema
+carregarSistema = do
+    existeDb <- doesFileExist dbPath
+    
+    if existeDb then do
+        conteudo <- withFile dbPath ReadMode $ \h -> do
+            hGetContents h
+       
+        case readMaybe conteudo of
+            Just sistemaLido -> do
+                putStrLn "Dados carregados!"
+                return sistemaLido
+                
+            Nothing -> do
+                putStrLn "Iniciando sistema vazio."
+                return sistemaVazio
+                
+    else do
+        putStrLn "Nenhum arquivo encontrado."
+        return sistemaVazio
+
+
+salvarSistema :: Sistema -> IO ()
+salvarSistema sistema = do
+    let temp = dbPath ++ ".tmp"
+    writeFile temp (show sistema)
+    renameFile temp dbPath
+    putStrLn "Sistema salvo."
