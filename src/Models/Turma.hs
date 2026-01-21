226509aab1dfbf63eba78d18cccfdf0e47379802
Iury <iury32211@outlook.com>
2026-01-21 14:06:58 -0300

    Ajuste na classe Turma

--- src/Models/Turma.hs
+++ src/Models/Turma.hs
@@ -1,4 +1,4 @@
-module Models.Turma(Turma, getCapacidadeTurma, getCodigoTurma, getAlunosTurma, getDisciplinaTurma, getHorarioTurma, getProfessorTurma, adicionarAlunoTurma, criarTurma, temVagaTurma) where
+module Models.Turma(Turma, getCapacidadeTurma, getCodigoTurma, getAlunosTurma, getDisciplinaTurma, getHorarioTurma, getProfessorTurma, adicionarAlunoTurma, criarTurma, temVagaTurma, getSalaTurma) where
 --Paulo adicionou o getCapacidadeTurma na parte de exportação para teste da IO
 import Models.Professor
 import Models.Aluno
@@ -9,16 +9,18 @@ data Turma = Turma{
   _matriculaProfessor :: Int,
   _disciplina :: String,
   _horario :: String,
+  _sala :: String,
   _alunos :: [Aluno],
   _qtdMaxAlunos :: Int
   } deriving (Show, Read, Eq)
 
-criarTurma :: Int -> Int -> String -> String -> Int -> Turma 
-criarTurma codigo professor disciplina horario qtdAlunos = Turma {
+criarTurma :: Int -> Int -> String -> String -> String -> Int -> Turma 
+criarTurma codigo professor disciplina horario sala qtdAlunos = Turma {
   _codigo = codigo,
   _matriculaProfessor = professor,
   _disciplina = disciplina,
   _horario = horario,
+  _sala = sala,
   _qtdMaxAlunos = qtdAlunos,
   _alunos = []
 }
@@ -45,6 +47,9 @@ getAlunosTurma = _alunos
 
 temVagaTurma :: Turma -> Bool
 temVagaTurma turma = length (_alunos turma) < _qtdMaxAlunos turma
+
+getSalaTurma :: Turma -> String
+getSalaTurma = _sala
 
 -- Paulo fez as alterações abaixo para testar a IO
 getCapacidadeTurma :: Turma -> Int
