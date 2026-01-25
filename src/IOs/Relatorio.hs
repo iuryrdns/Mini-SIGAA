fbb11463fb76d5f73a7edf4f497337d7ecd515a7
Iury <iury32211@outlook.com>
2026-01-25 02:48:48 -0300

    Atualiza função gerarRelatorioGeral

--- src/IOs/Relatorio.hs
+++ src/IOs/Relatorio.hs
@@ -10,18 +10,19 @@ import Models.Turma (getCodigoTurma, getDisciplinaTurma, getProfessorTurma, getH
 import Models.Types (unMatricula, unNome, unCodigo, unCRA, unCurso)
 
 gerarRelatorioGeral :: Sistema -> String
-gerarRelatorioGeral sistema = 
+gerarRelatorioGeral sistema =
     let alunos = Map.elems (getAlunos sistema)
         professores = Map.elems (getProfessores sistema)
         disciplinas = Map.elems (getDisciplinas sistema)
         turmas = Map.elems (getTurmas sistema)
         matriculas = _matriculas sistema
-        
+        fase = _fase sistema
+
         -- Helpers para buscar nomes
         buscaProf mat = case Map.lookup mat (getProfessores sistema) of
             Just p -> unNome (getNomeProfessor p)
             Nothing -> "Desconhecido"
-            
+
         buscaDisc cod = case Map.lookup cod (getDisciplinas sistema) of
             Just d -> unNome (getNomeDisciplina d)
             Nothing -> "Desconhecida"
@@ -32,35 +33,45 @@ gerarRelatorioGeral sistema =
 
         -- Formatação Alunos
         strAlunos = if null alunos then "Nenhum aluno cadastrado.\n" else
-            unlines [ show (unMatricula (getMatriculaAluno a)) ++ " - " ++ unNome (getNomeAluno a) ++ 
+            unlines [ show (unMatricula (getMatriculaAluno a)) ++ " - " ++ unNome (getNomeAluno a) ++
                       " (" ++ unCurso (getCursoAluno a) ++ ", CRA: " ++ show (unCRA (getCraAluno a)) ++ ")"
                     | a <- sortOn (unNome . getNomeAluno) alunos ]
 
         -- Formatação Professores
         strProfessores = if null professores then "Nenhum professor cadastrado.\n" else
-            unlines [ show (unMatricula (getMatriculaProfessor p)) ++ " - " ++ unNome (getNomeProfessor p) ++ 
+            unlines [ show (unMatricula (getMatriculaProfessor p)) ++ " - " ++ unNome (getNomeProfessor p) ++
                       " (" ++ getDepartamentoProfessor p ++ ", " ++ getFormacaoProfessor p ++ ")"
                     | p <- sortOn (unNome . getNomeProfessor) professores ]
 
         -- Formatação Disciplinas
         strDisciplinas = if null disciplinas then "Nenhuma disciplina cadastrada.\n" else
-            unlines [ unCodigo (getCodigoDisciplina d) ++ " - " ++ unNome (getNomeDisciplina d) ++ 
-                      "\n   Requisitos: " ++ (if null (getRequisitosDisciplina d) then "Nenhum" else intercalate ", " (map unCodigo (getRequisitosDisciplina d))) ++ 
+            unlines [ unCodigo (getCodigoDisciplina d) ++ " - " ++ unNome (getNomeDisciplina d) ++
+                      "\n   Requisitos: " ++ (if null (getRequisitosDisciplina d) then "Nenhum" else intercalate ", " (map unCodigo (getRequisitosDisciplina d))) ++
                       "\n   Cursos: " ++ intercalate ", " (map unCurso (getCursosDisciplina d))
                     | d <- sortOn (unNome . getNomeDisciplina) disciplinas ]
 
         -- Formatação Turmas
-        strTurmas = if null turmas then "Nenhuma turma cadastrada.\n" else
-            unlines [ "Turma " ++ show (getCodigoTurma t) ++ " - " ++ buscaDisc (getDisciplinaTurma t) ++ 
-                      "\n   Professor: " ++ buscaProf (getProfessorTurma t) ++ 
-                      "\n   Horário: " ++ show (getHorarioTurma t) ++ " | Sala: " ++ getSalaTurma t ++ 
-                      "\n   Capacidade: " ++ show (length (getAlunosTurma t)) ++ "/" ++ show (getCapacidadeTurma t)
-                    | t <- sortOn getCodigoTurma turmas ]
+        formatarAlunos t = 
+            let alunos = getAlunosTurma t
+             in if null alunos 
+                then "   Alunos: Nenhum aluno matriculado"
+                else "   Alunos:\n" ++ unlines [ "      - " ++ unNome (getNomeAluno a) ++ " (" ++ show (unMatricula (getMatriculaAluno a)) ++ ")"
+                                                 | a <- alunos ]
         
-        -- Formatação Matrículas
-        strMatriculas = if null matriculas then "Nenhuma matrícula efetivada.\n" else
+        strTurmas = if null turmas then "Nenhuma turma cadastrada.\n" else
+            unlines [ "Turma " ++ show (getCodigoTurma t) ++ " - " ++ buscaDisc (getDisciplinaTurma t) ++
+                      "\n   Professor: " ++ buscaProf (getProfessorTurma t) ++
+                      "\n   Horário: " ++  getHorarioTurma t ++ " | Sala: " ++ getSalaTurma t ++
+                      "\n   Capacidade: " ++ show (length (getAlunosTurma t)) ++ "/" ++ show (getCapacidadeTurma t) ++
+                      "\n" ++ formatarAlunos t
+                    | t <- sortOn getCodigoTurma turmas ]
+
+        -- Formatação Matrículas (só mostra se fase >= 2 e tem matrículas)
+        strMatriculas = if fase >= 2 && not (null matriculas) then
+            "--- MATRÍCULAS EFETIVADAS (" ++ show (length matriculas) ++ ") ---\n" ++
             unlines [ buscaAluno mat ++ " (" ++ show (unMatricula mat) ++ ") -> Turma " ++ show codTurma
                     | (mat, codTurma) <- matriculas ]
+            else ""
 
     in  "\n=========================================================\n" ++
         "          RELATÓRIO GERAL DO SISTEMA\n" ++
@@ -73,6 +84,5 @@ gerarRelatorioGeral sistema =
         strDisciplinas ++ "\n" ++
         "--- TURMAS CADASTRADAS (" ++ show (length turmas) ++ ") ---\n" ++
         strTurmas ++ "\n" ++
-        "--- MATRÍCULAS EFETIVADAS (" ++ show (length matriculas) ++ ") ---\n" ++
-        strMatriculas ++ 
+        strMatriculas ++
         "=========================================================\n"
