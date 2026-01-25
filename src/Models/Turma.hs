8dac5b812bcd59660ae52510e5f876481d6d64dd
Iury <iury32211@outlook.com>
2026-01-25 02:48:27 -0300

    Adiciona funções para limpar alunos e definir horário e sala na turma

--- src/Models/Turma.hs
+++ src/Models/Turma.hs
@@ -1,4 +1,4 @@
-module Models.Turma (Turma, getCapacidadeTurma, getCodigoTurma, getAlunosTurma, getDisciplinaTurma, getHorarioTurma, getProfessorTurma, adicionarAlunoTurma, criarTurma, getSalaTurma) where
+module Models.Turma (Turma, getCapacidadeTurma, getCodigoTurma, getAlunosTurma, getDisciplinaTurma, getHorarioTurma, getProfessorTurma, adicionarAlunoTurma, limparAlunosTurma, criarTurma, getSalaTurma, setHorarioTurma, setSalaTurma) where
 
 import Models.Aluno
 import Models.Disciplina
@@ -34,6 +34,9 @@ adicionarAlunoTurma turma aluno =
     { _alunos = _alunos turma ++ [aluno]
     }
 
+limparAlunosTurma :: Turma -> Turma
+limparAlunosTurma turma = turma { _alunos = [] }
+
 getCodigoTurma :: Turma -> Int
 getCodigoTurma = _codigo
 
@@ -51,6 +54,12 @@ getAlunosTurma = _alunos
 
 getSalaTurma :: Turma -> String
 getSalaTurma = _sala
+
+setHorarioTurma :: Turma -> Horario -> Turma
+setHorarioTurma turma novoHorario = turma {_horario = novoHorario}
+
+setSalaTurma :: Turma -> String -> Turma
+setSalaTurma turma novaSala = turma {_sala = novaSala}
 
 -- Paulo fez as alterações abaixo para testar a IO
 getCapacidadeTurma :: Turma -> Int
