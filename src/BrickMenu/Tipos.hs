5a06261e7da72807fac530d8b772dad2301f9816
Iury <iury32211@outlook.com>
2026-01-26 16:48:28 -0300

    Remove import desnecessário e reorganiza definições de tipos na estrutura de dados

--- src/BrickMenu/Tipos.hs
+++ src/BrickMenu/Tipos.hs
@@ -12,7 +12,6 @@ import Models.Aluno (Aluno)
 import Sistema (Sistema)
 import Models.Professor (Professor)
 import Models.Types (Matricula, Nome, Codigo, Curso)
-import Brick.Widgets.List (List)
 import Models.Turma (Turma)
 import Models.Disciplina (Disciplina)
 
@@ -21,35 +20,40 @@ data Name = MenuPrincipal
           | EditMatriculaProfessor | EditNomeProfessor | EditDepto | EditFormacao 
           | EditCodigoDisciplina | EditNomeDisciplina | EditRequisitos | EditCursosPermitidos
           | EditCodTurma | EditProfTurma | EditDiscTurma | EditHorario | EditSala | EditMaxAlunosTurma
-          | ListaAlunos
-          | ListaProfessores
-          | ListaTurmas
-          | ListaDisciplinas
           | EditMatAluno | EditMatTurma
-          | ListaSolicitacoes
+          | EditNotaMatricula | EditNotaCodDisc | EditNotaValor
+          | EditPendenteSala | EditPendenteHorario
+          | ListaAlunos | ListaProfessores | ListaTurmas | ListaDisciplinas | ListaSolicitacoes | ListaConflitos
           | ScrollRelatorio
-          | BtnSim | BtnNao
           deriving (Ord, Show, Eq)
 
-data Tela = TelaInicial | TelaMenu | TelaCadAluno | TelaCadProfessor | TelaCadDisciplina | TelaCadTurma 
+data Tela = TelaInicial 
+          | TelaMenu 
+          | TelaCadAluno | TelaCadProfessor | TelaCadDisciplina | TelaCadTurma 
           | TelaListaAlunos | TelaListaProfessores | TelaListaTurmas | TelaListaDisciplinas
-          | TelaMatriculas | TelaAgenda | TelaCadSolicitacao | TelaListaSolicitacoes
-          | TelaRelatorio | TelaConfirmacao | TelaConfirmacaoFimSemestre
+          | TelaConflitos 
+          | TelaEditarTurma 
+          | TelaCadSolicitacao | TelaListaSolicitacoes
+          | TelaLancarNotas
+          | TelaRelatorio 
+          | TelaConfirmacao | TelaConfirmacaoFimSemestre
     deriving (Eq, Show)
 
 data AppState = AppState
-  { _sistema     :: Sistema
-  , _listaMenu   :: L.List Name String
+  { _sistema       :: Sistema
+  , _listaMenu     :: L.List Name String
   , _listaMenuAlunos :: L.List Name Aluno
   , _listaMenuProfessores :: L.List Name Professor
   , _listaMenuTurmas :: L.List Name Turma
   , _listaMenuDisciplinas :: L.List Name Disciplina
-  , _listaMenuSolicitacoes :: L.List Name (Matricula, Int)
+  , _listaMenuSolicitacoes :: L.List Name (Matricula, Codigo)
+  , _listaConflitos :: L.List Name (Turma, Turma)
+  , _turmaEmEdicao :: Maybe Codigo
   , _textoRelatorio :: String
-  , _telaAtiva   :: Tela
-  , _mensagemErro :: Maybe String
-  , _formularios :: M.Map Name (Editor String Name)
-  , _foco        :: FocusRing Name
+  , _telaAtiva     :: Tela
+  , _mensagemErro  :: Maybe String
+  , _formularios   :: M.Map Name (Editor String Name)
+  , _foco          :: FocusRing Name
   }
 
 makeLenses ''AppState
